9f676e43fa50f8087917e46d6ac3b74afc331772
Jitsu <jitsu233@gmail.com>
2025-09-17 12:58:28 +0800

    change default translation cdn

--- MuvluvMod/Config.cs
+++ MuvluvMod/Config.cs
@@ -22,7 +22,7 @@ namespace MuvluvMod
             AutoSkipBattle = Plugin.Config.Bind("Gerneral", "AutoSkipBattle", false, "自动跳过战斗（自动按跳过键，不受跳过键开关影响）");
 
             Translation = Plugin.Config.Bind("Translation", "Enable", true, "是否开启汉化");
-            TranslationCDN = Plugin.Config.Bind("Translation", "CdnURL", "https://muvluvgg.ntr.best", "翻译加载的CDN");
+            TranslationCDN = Plugin.Config.Bind("Translation", "CdnURL", "https://raw.githubusercontent.com/anosu/muvluvgg-translation/refs/heads/main", "翻译加载的CDN");
             FontBundlePath = Plugin.Config.Bind("Translation", "FontBundlePath", "font/sarasagothicsc-bold", "TMP字体AssetBundle的路径");
             FontAssetName = Plugin.Config.Bind("Translation", "FontAssetName", "SarasaGothicSC-Bold SDF", "AssetBundle中TMP_FontAsset的名称");
 
